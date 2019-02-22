250b9c062b29bd6a28ac4ff60583155dbff33930
Szabó Gábor <szabogabor@hydrogene.hu>
2019-02-22 23:12:40 +0100

    ok

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,7 +1,14 @@
 // Load jQuery from NPM
-// import $ from 'jquery';
+// import $ from "jquery";
 // window.jQuery = $;
 // window.$ = $;
 
 // Add your scripts here
-
+var fuge = function nothing(params) {
+    var i = 1;
+    if (true) {
+        i++;
+    } else {
+        return i;
+    }
+};
