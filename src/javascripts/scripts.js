b7420feac279de7f3d8f3af7e9b61af8e1e0a600
Szabó Gábor <szabogabi@gmail.com>
2019-03-21 16:15:31 +0100

    sokk

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -3,6 +3,17 @@
 // window.jQuery = $;
 // window.$ = $;
 
+import Glide from "@glidejs/glide";
+//import Glide, { Controls, Breakpoints } from "@glidejs/glide/dist/glide.modular.esm";
+
+new Glide(".glide", {
+    type: "carousel",
+    startAt: 0,
+    perView: 1,
+    gap: 20
+    //focusAt: "center"
+}).mount();
+
 // Add your scripts here
 var fuge = function nothing(params) {
     var i = 1;
